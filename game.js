248469c546e0c983043a50657725649826c6f398
Syed Faizul Hai <faiz@iitj.ac.in>
2017-10-04 17:42:05 +0530

    Added two player (buggy)

--- game.js
+++ game.js
@@ -6,7 +6,11 @@ var ballSpeedX = 10;
 var ballSpeedY = 5;
 
 var paddle1Y = 250;
+var paddle2Y = 250;
 const PADDLE_HEIGHT = 100;
+const PADDLE_WIDTH = 10;
+
+const KEYBOARD_SENSTIVITY = 100;
 
 function calculateMousePos(evt) {
 	var rect = canvas.getBoundingClientRect();
@@ -16,6 +20,19 @@ function calculateMousePos(evt) {
 	return {
 		x: mouseX,
 		y: mouseY
+	}
+}
+
+function keyPressed(evt) {
+	switch (evt.keyCode) {
+		case 38: 
+			if (paddle2Y < 0) return;
+			paddle2Y = paddle2Y - KEYBOARD_SENSTIVITY;
+			break;
+		case 40:
+			if (paddle2Y >= canvas.height - PADDLE_HEIGHT) return;
+			paddle2Y = paddle2Y + KEYBOARD_SENSTIVITY;
+			break;
 	}
 }
 
@@ -29,6 +46,12 @@ window.onload = function(){
 		var mousePos = calculateMousePos(evt);
 		paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
 	});
+
+	setInterval((() => {
+		ballSpeedX += 2;
+		ballSpeedY += 1;
+	}), 10000);
+	document.addEventListener('keydown', keyPressed);
 }
 
 function ballReset() {
@@ -40,7 +63,11 @@ function ballReset() {
 function moveEverything() {
 	ballX += ballSpeedX;
 	if (ballX >= canvas.width) {
-		ballSpeedX = -ballSpeedX;
+		if (ballY > paddle2Y - 50 && ballY < (paddle2Y + PADDLE_HEIGHT + 50)) {
+			ballSpeedX = -ballSpeedX;
+		} else {
+			ballReset();
+		}
 	}
 	if (ballX <= 0) {
 		if (ballY > paddle1Y && ballY < (paddle1Y + PADDLE_HEIGHT)) {
@@ -64,7 +91,8 @@ function drawEverything() {
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
 
 	// left player paddle
-	colorRect(0, paddle1Y, 10, PADDLE_HEIGHT, 'white');
+	colorRect(0, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+	colorRect(canvas.width - PADDLE_WIDTH, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
 	
 	// ball
 	colorCircle(ballX, ballY, 10, 'white');
